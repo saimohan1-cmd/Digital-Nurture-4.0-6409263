452e8b1363fa59ea0c26343d0c2df7ac17879636
Saimohan <e.saimohan04@gmail.com>
2025-08-10 20:02:10 +0530

    Save changes in Week 7 Exercise 13

--- Week 7/Exercise 13/bloggerapp/src/components/BookDetails.js
+++ Week 7/Exercise 13/bloggerapp/src/components/BookDetails.js
@@ -3,13 +3,15 @@ import React from 'react'
 export default function BookDetails({books}) {
   return (
     <div>
-      <h2>Book Details</h2>
-      {books.map(book=>(
+    <ul>
+        {books.map(book=>(
         <div key={book.id}>
         <h3>{book.bname}</h3>
         <h4>{book.price}</h4>
         </div>
       ))}
+    </ul>
+      
     </div>
   )
 }
